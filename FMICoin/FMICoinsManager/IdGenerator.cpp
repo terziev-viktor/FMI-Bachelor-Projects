3bb51a701e8e6304183a4dde039a06f5bbba9341
terziev-viktor <terzievviktor@gmail.com>
2018-03-07 20:08:01 +0200

    Writing struct to file and reading wallet info from file

--- FMICoin/FMICoinsManager/IdGenerator.cpp
+++ FMICoin/FMICoinsManager/IdGenerator.cpp
@@ -3,7 +3,7 @@
 unsigned int GenerateId()
 {
 	unsigned int id = 0;
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 200; i++)
 	{
 		int bit = rand() % 33;
 		id = id ^ (1 << bit);
--- FMICoin/FMICoinsManager/Main.cpp
+++ FMICoin/FMICoinsManager/Main.cpp
@@ -4,7 +4,6 @@
 
 #include "IdGenerator.h"
 #include "Models.h"
-
 using std::cin;
 using std::cout;
 using std::endl;
@@ -17,6 +16,13 @@ const char ORDERS[] = "orders.dat";
 const char COMMAND_QUIT[] = "quit";
 const char COMMAND_ADD_WALLET[] = "add-wallet";
 const char COMMAND_MAKE_ORDER[] = "make-order";
+const char COMMAND_WALLET_INFO[] = "wallet-info";
+const char COMMAND_ATTRACT_INVESTORS[] = "attract-investors";
+
+void print(const Wallet & w)
+{
+	cout << w.id << " " << w.fiatMoney << " " << w.owner << endl;
+}
 
 bool AddWallet()
 {
@@ -36,13 +42,11 @@ bool AddWallet()
 	cin.getline(owner, 256);
 
 	unsigned int id = GenerateId();
-	// save the new wallet -> id, fiatmoney, owner
-	file.write(reinterpret_cast<char*>(&id), sizeof(unsigned int));
-	file.write(reinterpret_cast<char*>(&fiatMoney), sizeof(double));
-	int sizeofowner = sizeof(owner);
-	file.write(reinterpret_cast<char*>(&sizeofowner), sizeof(int));
-	file.write(reinterpret_cast<char*>(&owner), sizeofowner);
-
+	Wallet w;
+	w.id = id;
+	w.fiatMoney = fiatMoney;
+	strcpy_s(w.owner, owner);
+	file.write(reinterpret_cast<char*>(&w), sizeof(Wallet));
 	file.close();
 	return true;
 }
@@ -80,7 +84,7 @@ bool MakeOrder()
 	// Продай ги
 	// Ако не могат да бъдат продадени ги пази в orders.dat
 	// Създай съответните транзакции
-
+	
 	int sizeoftype = sizeof(type);
 	file.write(reinterpret_cast<char*>(&sizeoftype), sizeof(int));
 	file.write(reinterpret_cast<char*>(&type), sizeoftype);
@@ -91,56 +95,120 @@ bool MakeOrder()
 	return true;
 }
 
+bool WalletInfo()
+{
+	unsigned int id;
+	cin >> id;
+	std::ifstream file;
+	file.open(WALLETS, std::ios::binary);
+	if (!file.good())
+	{
+		file.close();
+		return false;
+	}
+
+	while (file.eof())
+	{
+		Wallet w;
+		file.read(reinterpret_cast<char*>(&w), sizeof(Wallet));
+		if (w.id == id)
+		{
+			print(w);
+			file.close();
+			return true;
+		}
+	}
+	cout << "Wallet doesn't exist." << endl;
+	file.close();
+	return true;
+}
+
+bool AttractInvestors()
+{
+	// TODO:
+	return false;
+}
+
 int main()
 {
 	srand(time(NULL)); // set random seed for id generating function
 	std::ifstream file;
 	file.open(WALLETS, std::ios::binary);
-	
+
 	if (file.good())
 	{
 		while (!file.eof())
 		{
-			int id;
-			file.read( reinterpret_cast<char*>(&id), sizeof(int));
-			cout << id << endl;
-			double fiatMoney;
-			file.read( reinterpret_cast<char*>(&fiatMoney), sizeof(double));
-			cout << fiatMoney << endl;
-			int sizeofowner;
-			file.read(reinterpret_cast<char*>(&sizeofowner), sizeof(int));
-			cout << sizeofowner << endl;
-			char owner[256];
-			file.read(reinterpret_cast<char*>(&owner), sizeofowner);
-			cout << owner << endl;
+			Wallet w;
+			file.read(reinterpret_cast<char*>(&w), sizeof(Wallet));
+			if (file.eof())
+			{
+				break;
+			}
+			print(w);
 		}
 	}
 
 	file.close();
 
 	char command[18];
-	cin >> command;
-	while (strcmp(command, COMMAND_QUIT) != 0)
+	do
 	{
+		cin >> command;
+
 		if (strcmp(command, COMMAND_ADD_WALLET) == 0)
 		{
 			bool success = AddWallet();
-			if (!success)
+			if (success)
+			{
+				cout << "Wallet Added." << endl;
+			}
+			else
 			{
 				cout << "Adding wallet failed." << endl;
 			}
+			continue;
 		}
+
 		if (strcmp(command, COMMAND_MAKE_ORDER) == 0)
 		{
 			bool success = MakeOrder();
-			if (!success)
+			if (success)
+			{
+				cout << "New order created." << endl;
+			}
+			else
 			{
 				cout << "Making order failed" << endl;
 			}
+			continue;
 		}
 
-	}
+		if (strcmp(command, COMMAND_WALLET_INFO) == 0)
+		{
+			bool success = WalletInfo();
+			if (success)
+			{
+				cout << "Wallet:" << endl;
+			}
+			else
+			{
+				cout << "Viewing wallet info failed" << endl;
+			}
+			continue;
+		}
+		
+		if (strcmp(command, COMMAND_ATTRACT_INVESTORS) == 0)
+		{
+			bool success = AttractInvestors();
+			if (!success)
+			{
+				cout << "Attracting investors failed" << endl;
+			}
+			continue;
+		}
 
+	} while (strcmp(command, COMMAND_QUIT) != 0);
 
 	return 0;
 }
