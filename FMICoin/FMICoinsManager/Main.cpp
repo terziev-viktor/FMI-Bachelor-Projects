f7c5541eb7b247acee97c5242addb441b2f2c713
terziev-viktor <terzievviktor@gmail.com>
2018-03-10 00:56:21 +0200

    Writing transaction for every newly crated wallet

--- FMICoin/FMICoinsManager/Main.cpp
+++ FMICoin/FMICoinsManager/Main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <time.h>
+#include <ctime>
+#include <Windows.h>
 
 #include "IdGenerator.h"
 #include "Models.h"
 using std::cin;
 using std::cout;
 using std::endl;
-
+// Consts
+const int CONSOLE_GREEN = 10;
+const int CONSOLE_DEFAULT = 7;
+const int CONSOLE_RED = 12;
 const int LEV_TO_FMICOINT_COURCE = 375;
-
+const unsigned int SYSTEM_WALLET = 4294967295;
+// Files
 const char WALLETS[] = "wallets.dat";
 const char ORDERS[] = "orders.dat";
-
+const char TRANSACTIONS[] = "transactions.dat";
+// Commands
 const char COMMAND_QUIT[] = "quit";
 const char COMMAND_ADD_WALLET[] = "add-wallet";
 const char COMMAND_MAKE_ORDER[] = "make-order";
@@ -22,6 +28,23 @@ const char COMMAND_ATTRACT_INVESTORS[] = "attract-investors";
 void print(const Wallet & w)
 {
 	cout << w.id << " " << w.fiatMoney << " " << w.owner << endl;
+}
+
+bool MakeTransaction(unsigned int senderId, unsigned int recieverId, double coins)
+{
+	std::ofstream file;
+	file.open(TRANSACTIONS, std::ios::binary | std::ios::app);
+	if (!file.good())
+	{
+		file.close();
+		return false;
+	}
+	file.write(reinterpret_cast<char*>(&senderId), sizeof(unsigned int));
+	file.write(reinterpret_cast<char*>(&recieverId), sizeof(unsigned int));
+	file.write(reinterpret_cast<char*>(&coins), sizeof(double));
+
+	file.close();
+	return true;
 }
 
 bool AddWallet()
@@ -34,20 +57,26 @@ bool AddWallet()
 		return false;
 	}
 
-	double fiatMoney;
+	double fiatMoney, coins;
 	cin >> fiatMoney;
+	coins = fiatMoney / LEV_TO_FMICOINT_COURCE;
 
 	char owner[256];
 	cin.ignore();
 	cin.getline(owner, 256);
 
 	unsigned int id = GenerateId();
-	Wallet w;
-	w.id = id;
-	w.fiatMoney = fiatMoney;
-	strcpy_s(w.owner, owner);
-	file.write(reinterpret_cast<char*>(&w), sizeof(Wallet));
+	
+	// write to wallets.dat 
+	// id -> fiatmoney -> owner [unsigned int][double][256 bytes]
+	file.write(reinterpret_cast<char*>(&id), sizeof(unsigned int));
+	file.write(reinterpret_cast<char*>(&fiatMoney), sizeof(double));
+	file.write(reinterpret_cast<char*>(&owner), sizeof(char) * 256);
 	file.close();
+
+	// Put coins in the new wallet
+	MakeTransaction(SYSTEM_WALLET, id, coins);
+
 	return true;
 }
 
@@ -73,17 +102,6 @@ bool MakeOrder()
 	cin >> walletId;
 
 	// TODO
-	// Покупка:
-	// Провери дали портфейла има достатъчно пари за монетите които ще купува
-	// Удовлетвори нареждането за покупка
-	// Ако не може да бъде удовлетворено напълно го запази в orders.dat
-	// Създай съответните транзакции
-	//
-	// Продажба:
-	// Провери дали в портвейла има достатъчно монети за продажба
-	// Продай ги
-	// Ако не могат да бъдат продадени ги пази в orders.dat
-	// Създай съответните транзакции
 	
 	int sizeoftype = sizeof(type);
 	file.write(reinterpret_cast<char*>(&sizeoftype), sizeof(int));
@@ -129,28 +147,8 @@ bool AttractInvestors()
 	return false;
 }
 
-int main()
+void Run(HANDLE &hConsole)
 {
-	srand(time(NULL)); // set random seed for id generating function
-	std::ifstream file;
-	file.open(WALLETS, std::ios::binary);
-
-	if (file.good())
-	{
-		while (!file.eof())
-		{
-			Wallet w;
-			file.read(reinterpret_cast<char*>(&w), sizeof(Wallet));
-			if (file.eof())
-			{
-				break;
-			}
-			print(w);
-		}
-	}
-
-	file.close();
-
 	char command[18];
 	do
 	{
@@ -161,11 +159,15 @@ int main()
 			bool success = AddWallet();
 			if (success)
 			{
+				SetConsoleTextAttribute(hConsole, CONSOLE_GREEN);
 				cout << "Wallet Added." << endl;
+				SetConsoleTextAttribute(hConsole, CONSOLE_DEFAULT);
 			}
 			else
 			{
+				SetConsoleTextAttribute(hConsole, CONSOLE_RED);
 				cout << "Adding wallet failed." << endl;
+				SetConsoleTextAttribute(hConsole, CONSOLE_DEFAULT);
 			}
 			continue;
 		}
@@ -175,11 +177,15 @@ int main()
 			bool success = MakeOrder();
 			if (success)
 			{
+				SetConsoleTextAttribute(hConsole, CONSOLE_GREEN);
 				cout << "New order created." << endl;
+				SetConsoleTextAttribute(hConsole, CONSOLE_DEFAULT);
 			}
 			else
 			{
+				SetConsoleTextAttribute(hConsole, CONSOLE_RED);
 				cout << "Making order failed" << endl;
+				SetConsoleTextAttribute(hConsole, CONSOLE_DEFAULT);
 			}
 			continue;
 		}
@@ -197,7 +203,7 @@ int main()
 			}
 			continue;
 		}
-		
+
 		if (strcmp(command, COMMAND_ATTRACT_INVESTORS) == 0)
 		{
 			bool success = AttractInvestors();
@@ -209,6 +215,62 @@ int main()
 		}
 
 	} while (strcmp(command, COMMAND_QUIT) != 0);
+}
+
+void Load()
+{
+	std::ifstream file;
+	file.open(WALLETS, std::ios::binary);
+
+	if (file.good())
+	{
+		unsigned int id;
+		double money;
+		char name[256];
+		while (!file.eof())
+		{
+			file.read(reinterpret_cast<char*>(&id), sizeof(unsigned int));
+			file.read(reinterpret_cast<char*>(&money), sizeof(double));
+			file.read(reinterpret_cast<char*>(&name), sizeof(char) * 256);
+			if (!file.eof())
+			{
+				cout << id << " " << money << " " << name << endl;
+			}
+		}
+	}
+	file.close();
+
+	std::ifstream transactions;
+	transactions.open(TRANSACTIONS, std::ios::binary);
+	cout << endl;
+	if (transactions.good())
+	{
+		unsigned sender;
+		unsigned reciever;
+		double coins;
+		while (!transactions.eof())
+		{
+			transactions.read(reinterpret_cast<char*>(&sender), sizeof(sender));
+			transactions.read(reinterpret_cast<char*>(&reciever), sizeof(reciever));
+			transactions.read(reinterpret_cast<char*>(&coins), sizeof(double));
+			if (!transactions.eof())
+			{
+				cout << sender << "->" << reciever << " " << coins << endl;
+			}
+		}
+	}
+	file.close();
+}
+
+int main()
+{
+	srand(time(NULL)); // set random seed for id generating function
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Load();
+
+	Run(hConsole);
 
 	return 0;
 }
