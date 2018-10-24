6c2b4f2a726368bd8f7ae66506f16924cd9e9741
terziev-viktor <terzievviktor@gmail.com>
2018-10-24 20:22:57 +0300

    Added iterator and some fun methods

--- DataStructures/LinkedList/Konga/Konga/SinglyLinkedList.hpp
+++ DataStructures/LinkedList/Konga/Konga/SinglyLinkedList.hpp
@@ -3,33 +3,158 @@
 template<typename T>
 class SinglyLinkedList
 {
-public:
-	SinglyLinkedList()
+protected:
+	class Node
 	{
-		this->_root = nullptr;
+	private:
+		Node * next;
+		T data;
+	public:
+		Node()
+			:next(nullptr)
+		{
+
+		}
+		Node(const T & value)
+		{
+			this->setValue(value);
+			this->next = nullptr;
+		}
+		~Node()
+		{
+			if (this->next)
+			{
+				delete this->next;
+			}
+		}
+
+		void setValue(const T & value)
+		{
+			this->data = value;
+		}
+
+		T getValue() const
+		{
+			return this->data;
+		}
+
+		T & getValue()
+		{
+			return this->data;
+		}
+
+		void setNext(Node * next)
+		{
+			if (this == next)
+			{
+				throw "no logic exception";
+			}
+			this->next = next;
+		}
+		Node * getNext() const
+		{
+			return this->next;
+		}
+	};
+
+private:
+	Node * first;
+	Node * last;
+	size_t count;
+public:
+	class Iterator
+	{
+	private:
+		Node * start;
+		Node * current;
+		bool done;
+	public:
+		Iterator(Node * start)
+		{
+			this->start = start;
+			this->current = this->start;
+			this->done = false;
+		}
+		bool isDone() const
+		{
+			return this->done;
+		}
+		T & getCurrent()
+		{
+			return this->current->getValue();
+		}
+		T getFirst() const
+		{
+			this->start->getValue();
+		}
+		bool operator()() const
+		{
+			return !this->done;
+		}
+		T operator*() const
+		{
+			return this->current->getValue();
+		}
+		T & operator*()
+		{
+			return this->current->getValue();
+		}
+		Iterator & operator->()
+		{
+			return *this;
+		}
+		
+		Iterator & operator++()
+		{
+			if (this->current == nullptr || this->current->getNext() == nullptr)
+			{
+				this->done = true;
+				return *this;
+			}
+			this->current = this->current->getNext();
+			return *this;
+		}
+		Iterator operator++(int)
+		{
+			if (this->current == nullptr || this->current->getNext() == nullptr)
+			{
+				this->done = true;
+				return *this;
+			}
+			Iterator tmp(this->current);
+			this->current = this->current->getNext();
+			return tmp;
+		}
+
+	};
+
+	SinglyLinkedList()
+		:first(nullptr), last(nullptr), count(0)
+	{
+
 	}
+
 	~SinglyLinkedList()
 	{
-		if (this->_root)
+		if (this->first)
 		{
-			delete this->_root;
+			delete this->first;
 		}
 	}
-	SinglyLinkedList & Add(const T & value)
+
+	T getFirst() const
 	{
-		Node * n = new Node();
-		n->setData(value);
-		n->setNext(this->_root);
-		this->_root = n;
-		return *this;
+		return this->first->getValue();
 	}
-	T getRoot() const
+
+	T getLast() const
 	{
-		return this->_root->getData();
+		return this->last->getValue();
 	}
+
 	T getAt(size_t index) const
 	{
-		Node * tmp = this->_root;
+		Node * tmp = this->first;
 		for (size_t i = 0; i < index; ++i)
 		{
 			if (tmp->getNext() == nullptr)
@@ -38,55 +163,81 @@ public:
 			}
 			tmp = tmp->getNext();
 		}
-		return tmp->getData();
+		return tmp->getValue();
 	}
-protected:
-	class Node
+
+	size_t getCount() const
 	{
-	public:
-		Node()
-			:_next(nullptr)
-		{
+		return this->count;
+	}
 
-		}
-		Node(const T & value)
+	SinglyLinkedList & pushBack(const T & value)
+	{
+		if (first == nullptr)
 		{
-			this->setValue(value);
-			this->_next = nullptr;
+			this->first = new Node(value);
+			this->last = this->first;
 		}
-		~Node()
+		else
 		{
-			if (this->_next)
+			this->last->setNext(new Node(value));
+			this->last = this->last->getNext();
+		}
+		++this->count;
+		return *this;
+	}
+
+	SinglyLinkedList & pushFront(const T & value)
+	{
+		if (this->first == nullptr)
+		{
+			this->first = new Node(value);
+			this->last = this->first;
+		}
+		else
+		{
+			Node * tmp = new Node(value);
+			tmp->setNext(this->first);
+			this->first = tmp;
+		}
+		+this->count;
+		return *this;
+	}
+
+	SinglyLinkedList & removeFront()
+	{
+		Node * tmp = this->first;
+		this->first = this->first->getNext();
+		tmp->setNext(nullptr);
+		delete tmp;
+		return *this;
+	}
+
+	Iterator begin() const
+	{
+		return Iterator(this->first);
+	}
+
+	// Implemented just for fun ----------------
+	void forEach(void(*func)(T & n))
+	{
+		for (Iterator i = this->begin(); !i.isDone(); ++i)
+		{
+			func(*i);
+		}
+	}
+
+	bool findFirst(bool(*condition)(const T & n), T & out) const
+	{
+		for (Iterator i = this->begin(); !i.isDone(); ++i)
+		{
+			if (condition(*i))
 			{
-				delete this->_next;
+				out = *i;
+				return true;
 			}
 		}
-
-		void setData(const T & value)
-		{
-			this->_data = value;
-		}
-		T getData() const
-		{
-			return this->_data;
-		}
-		void setNext(Node * next)
-		{
-			if (this == next)
-			{
-				throw "no logic exception";
-			}
-			this->_next = next;
-		}
-		Node * getNext() const
-		{
-			return this->_next;
-		}
-	private:
-		Node * _next;
-		T _data;
-	};
-
-private:
-	Node * _root;
+		return false;
+	}
+	// --------------------------------------------
 };
--- DataStructures/LinkedList/Konga/Konga/main.cpp
+++ DataStructures/LinkedList/Konga/Konga/main.cpp
@@ -5,10 +5,6 @@ using namespace std;
 
 int main()
 {
-	SinglyLinkedList<double> list;
-	list.Add(222.2).Add(33.3).Add(1.1);
-	double a = list.getAt(2);
-	cout << a << endl;
-
+	
 	return 0;
 }
