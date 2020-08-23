e33300c159d07a93c440a6941738559a71bd4d31
Viktor Terziev <terzievviktor@gmail.com>
2020-08-23 13:16:00 +0300

    Update forksandpipes.cpp

--- OS/forksandpipes.cpp
+++ OS/forksandpipes.cpp
@@ -1,9 +1,10 @@
 // gcc version 10.1.0
 // gcc -std=c++2a -fconcepts -O3 forksandpipes.cpp
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 auto main() -> int
 {
@@ -12,6 +13,7 @@ auto main() -> int
     const char* msg1 = "hello, world #1"; 
     const char* msg2 = "hello, world #2"; 
     const char* msg3 = "hello, world #3";
+    char buffer[MSGSIZE];
 
     // Create a pipe to use for communication
     if(pipe(the_pipe) < 0)
@@ -24,27 +26,30 @@ auto main() -> int
 
     if(pid_t pid = fork(); pid == 0) // we are the child process
     {
-        printf("Im the child process man! My id is %d \n", pid);
+        printf("Im the child process man! Writing some shit to the pipe!\n");
         write(pipe_write, msg1, MSGSIZE);
+        sleep(1); // doing some hard, hard work
         write(pipe_write, msg2, MSGSIZE);
+        sleep(1); // doint some hard, hard work
         write(pipe_write, msg3, MSGSIZE);
+
+        exit(0);
     }
     else // we are the parent process
     {
-        puts("Im the parent process man!");
+        //int status = -1;
+        //pid_t dead_pid = wait(&status); // wait for a process to die
+        printf("Child id is %d \n", pid);
+        
         int nbytes = 0;
-        char inbuffer[MSGSIZE];
-
-        while(nbytes = read(pipe_read, inbuffer, MSGSIZE))
+        int messages_to_read = 3; // we know we expect 3 messages in the pipe
+        while(messages_to_read-- && (nbytes = read(pipe_read, buffer, MSGSIZE)) > 0)
         {
-            printf("%s \n", inbuffer);
+            printf("%s \n", buffer);
         }
-        if(nbytes != 0)
-        {
-            return 2;
-        }
+        
         puts("Finised reading!");
-    }
 
-    return 0;
+        exit(0);
+    }
 }   
