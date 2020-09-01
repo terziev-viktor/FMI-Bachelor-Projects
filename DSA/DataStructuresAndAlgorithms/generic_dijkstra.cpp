18d2051f004c2bc3c4db9e74a132fabc3a87f5a6
Viktor Terziev <terzievviktor@gmail.com>
2020-09-01 14:19:27 +0300

    Create generic_dijkstra.cpp

---
+++ DSA/DataStructuresAndAlgorithms/generic_dijkstra.cpp
@@ -0,0 +1,145 @@
+// x86_64 gcc 10.1
+// gcc -std=c++2a -fconcepts -Os generic_dijkstra.cpp
+
+#include <iostream>
+#include <concepts>
+#include <algorithm>
+#include <tuple>
+#include <utility>
+#include <climits>
+#include <queue>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+auto dijkstra(forward_iterator auto V_begin, forward_iterator auto V_end, 
+                forward_iterator auto E_begin, forward_iterator auto E_end, 
+                auto w, 
+                decltype(V_begin) S,
+                auto adj) requires std::invocable<decltype(w), decltype(E_begin)>
+{
+    using v_type = decltype(V_begin);
+    using e_type = decay_t<decltype(*E_begin)>;
+    using w_type = decltype(w);
+
+    auto d_comparator = [V_begin](v_type v1, v_type v2) 
+    { 
+        return distance(V_begin, v1) < distance(V_begin, v2); 
+    };
+
+    using d_comparator_t = decltype(d_comparator);
+
+    vector<float> d(distance(V_begin, V_end), INT_MAX);
+    d[distance(V_begin, S)] = 0;
+    
+    // Priority queue of Vs
+    priority_queue<v_type, vector<v_type>, d_comparator_t> Q(d_comparator);
+
+    Q.push(S); // The first V that we will process
+    
+    while(Q.empty() == false)
+    {
+        v_type v = Q.top();
+        Q.pop();
+        
+        for(auto & u : adj(v))
+        {
+            e_type e { v, u };
+            auto e_iter = find(E_begin, E_end, e);
+            int v_index = distance(V_begin, v);
+            int u_index = distance(V_begin, u);
+
+            if(float trial_w = d[v_index] + w(e_iter); d[u_index] > trial_w)
+            {
+                d[u_index] = trial_w;
+                Q.push(u);
+            }
+        }
+    }
+
+    return d;
+}
+
+std::ostream & operator<<(std::ostream & out, const pair<int, int> & p)
+{
+    out << "{ " << p.first << ", " << p.second << " }";
+    return out;
+}
+
+auto main() -> int
+{
+    using Vs_type = array<int, 4>;
+    using V_type = Vs_type::iterator;
+    
+    using Es_type = array<pair<V_type, V_type>, 5>;
+    using E_type = Es_type::iterator;
+    
+    Vs_type V = { 1, 2, 3, 4 };
+
+    Es_type E = 
+    { 
+        pair<V_type, V_type>{ begin(V), begin(V) + 1 }, 
+                            { begin(V), begin(V) + 2 }, 
+                            { begin(V) + 1, begin(V) + 2 }, 
+                            { begin(V) + 1, begin(V) + 3 }, 
+                            { begin(V) + 3, begin(V) }
+    };
+
+    vector<pair<V_type, vector<V_type>>> Adjs
+    {
+        { begin(V), { begin(V) + 1, begin(V) + 2 } },
+        { begin(V) + 1, { begin(V) + 2, begin(V) + 3 } },
+        { begin(V) + 3, { begin(V) } }
+    };
+
+    /*      3
+        ---->-----
+        |        |
+       (1) ->(2)-(3)
+        |    |
+        ^    v
+        |---(4)
+    */
+
+    array<float, 5> weights = { 1.f, 3.f, 1.5f, 1.f, 1.f };
+
+    auto w = [Es_begin = begin(E), weights](E_type e) 
+    {
+        int index = distance(Es_begin, e);
+        if(index < 0)
+        {
+            puts("Wrong call of weight function");
+            return 0.f;
+        }
+        return weights[index];
+    };
+
+    auto adj = [&Adjs](V_type v)
+    {
+        vector<V_type> v_adj;
+        for(auto & a : Adjs)
+        {
+            if(a.first == v)
+            {
+                v_adj = a.second;
+                return v_adj;
+            }
+        }
+        return v_adj;
+    };
+
+    auto t1 = chrono::high_resolution_clock::now();
+    
+    auto d = dijkstra(begin(V), end(V), begin(E), end(E), w, begin(V), adj);
+
+    auto t2 = chrono::high_resolution_clock::now();
+
+    cout << "dijkstra took " << duration_cast<duration<double, micro>>(t2 - t1).count() << " microseconds \n";
+
+    for(auto & v : d)
+    {
+        cout << v << endl;
+    }
+
+    return 0;
+}
